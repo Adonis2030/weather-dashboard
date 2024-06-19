43c567f3d5ca7015cfa0e7785de8469a588f6228
Adonis2030 <service.provider2030@gmail.com>
2024-06-19 10:57:51 +0200

    add toastify notification

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const WeatherForm = ({ onFetchWeather }) => {
   const [location, setLocation] = useState({ lat: "", lon: "" });
@@ -6,41 +8,53 @@ const WeatherForm = ({ onFetchWeather }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    onFetchWeather(location, date);
+    if (!location.lat || !location.lon || !date) {
+      toast.error("All fields are required.", {
+        position: "top-right",
+        autoClose: 2000,
+        pauseOnHover: true,
+        theme: "dark",
+      });
+    } else {
+      onFetchWeather(location, date);
+    }
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="mb-5 py-5 flex-wrap flex-important justify-center gap-5"
-    >
-      <input
-        type="number"
-        placeholder="Latitude"
-        value={location.lat}
-        onChange={(e) => setLocation({ ...location, lat: e.target.value })}
-        className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
-      />
-      <input
-        type="number"
-        placeholder="Longitude"
-        value={location.lon}
-        onChange={(e) => setLocation({ ...location, lon: e.target.value })}
-        className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
-      />
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
-      />
-      <button
-        type="submit"
-        className="py-2 px-5 border block rounded-2xl bg-blue-900 text-white sm:w-full md:w-1/4 lg:w-auto w-full"
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="mb-5 py-5 flex-wrap flex-important justify-center gap-5"
       >
-        View Weather
-      </button>
-    </form>
+        <input
+          type="number"
+          placeholder="Latitude"
+          value={location.lat}
+          onChange={(e) => setLocation({ ...location, lat: e.target.value })}
+          className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
+        />
+        <input
+          type="number"
+          placeholder="Longitude"
+          value={location.lon}
+          onChange={(e) => setLocation({ ...location, lon: e.target.value })}
+          className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          className="py-2 px-5 border block rounded-2xl sm:w-full md:w-1/4 lg:w-auto w-full"
+        />
+        <button
+          type="submit"
+          className="py-2 px-5 border block rounded-2xl bg-blue-900 text-white sm:w-full md:w-1/4 lg:w-auto w-full"
+        >
+          View Weather
+        </button>
+      </form>
+      <ToastContainer />
+    </>
   );
 };
 
